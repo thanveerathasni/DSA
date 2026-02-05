60f82be6c2d3e14bdbff282b03a231394999a852
your-name <your-email>
2026-02-05 17:35:58 +0530

    added insertion sort practice

--- Sorting-Algorithms/SortingPractice.js
+++ Sorting-Algorithms/SortingPractice.js
@@ -19,18 +19,47 @@
 
 // selection sort
 
-let arr = [3,5,32,7,74,1]
+// let arr = [3,5,32,7,74,1]
 
-for(let i = 0;i< arr.length;i++){
-  let min = i ;
-  for(let j = i+1;j<arr.length ; j++){
+// for(let i = 0;i< arr.length;i++){
+//   let min = i ;
+//   for(let j = i+1;j<arr.length ; j++){
     
-    if(arr[j]<arr[min]){
-      min = j
-    }
-  }
+//     if(arr[j]<arr[min]){
+//       min = j
+//     }
+//   }
   
- [ arr[min],arr[i]] =[ arr[i],arr[min]]
-}
+//  [ arr[min],arr[i]] =[ arr[i],arr[min]]
+// }
 
+// console.log(arr)
+
+
+
+
+// insertionSort
+
+
+
+let arr = [2,4,1,5,25,6]
+
+for(let i = 1 ; i <arr.length;i++){
+  
+  let key = arr[i]
+  let j = i-1
+  
+  while(j>=0&&arr[j]>key){
+    arr[j+1] = arr[j]
+    j--
+    
+  }
+   arr[j+1] = key
+}
 console.log(arr)
+
+
+
+
+
+
