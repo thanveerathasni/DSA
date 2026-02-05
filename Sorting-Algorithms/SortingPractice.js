74b89aa0146b0b50cb199a77136c0ba1791b6dec
your-name <your-email>
2026-02-05 18:06:46 +0530

    added quick sort practice

--- Sorting-Algorithms/SortingPractice.js
+++ Sorting-Algorithms/SortingPractice.js
@@ -61,5 +61,35 @@
 
 
 
+//quickSort
+
+
+let arr = [2,8,1,4,53,9,20,21,23]
+function quickSort(arr){
+  
+  if(arr.length<=1) return arr
+  
+  let pivot = arr[arr.length-1]
+  let left = [] , right =[]
+  
+  for(let i=0 ;i < arr.length-1 ;i++){
+    if(arr[i]>pivot){
+      right.push(arr[i])
+    }else{
+      left.push(arr[i])
+    }
+    
+  }
+  
+  return [...quickSort(left),pivot,...quickSort(right)]
+  
+  
+}
+
+
+console.log(quickSort(arr))
+
+
+
 
 
