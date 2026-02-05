0add9e4cf514330b42d593848717dc6c553f52b8
your-name <your-email>
2026-02-05 17:21:09 +0530

    added selection practice

--- Sorting-Algorithms/SortingPractice.js
+++ Sorting-Algorithms/SortingPractice.js
@@ -2,17 +2,35 @@
 // bubble sort 
 
 
-let arr = [1,3,2,5,6,4]
+// let arr = [1,3,2,5,6,4]
 
-for(let i =0 ; i<arr.length ;i ++){
+// for(let i =0 ; i<arr.length ;i ++){
   
-  for(let j = 0 ; j<arr.length-i-1;j++){
-    if(arr[j]>arr[j+1]){
-      [arr[j],arr[j+1]] = [arr[j+1],arr[j]]
+//   for(let j = 0 ; j<arr.length-i-1;j++){
+//     if(arr[j]>arr[j+1]){
+//       [arr[j],arr[j+1]] = [arr[j+1],arr[j]]
+//     }
+//   }
+// }
+
+// console.log(arr)
+
+
+
+// selection sort
+
+let arr = [3,5,32,7,74,1]
+
+for(let i = 0;i< arr.length;i++){
+  let min = i ;
+  for(let j = i+1;j<arr.length ; j++){
+    
+    if(arr[j]<arr[min]){
+      min = j
     }
   }
+  
+ [ arr[min],arr[i]] =[ arr[i],arr[min]]
 }
 
 console.log(arr)
-
-
